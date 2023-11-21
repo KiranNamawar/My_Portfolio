f8a300944a5e3a1e0144618bc818335f41f62628
Kiran Namawar <kirannamawar@gmail.com>
2023-11-21 11:58:36 +0530

    Add script.js to index.html

---
+++ script.js
@@ -0,0 +1,20 @@
+
+// Function to display a welcome message
+function displayWelcomeMessage() {
+  console.log("Welcome to my portfolio!");
+}
+
+// Function to calculate the total number of projects
+function calculateTotalProjects(projects) {
+  return projects.length;
+}
+
+// Array to store project names
+var projects = ["Project 1", "Project 2", "Project 3"];
+
+// Display the welcome message
+displayWelcomeMessage();
+
+// Calculate and display the total number of projects
+var totalProjects = calculateTotalProjects(projects);
+console.log("Total projects: " + totalProjects);
