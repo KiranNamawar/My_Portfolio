f54c13187391b585adda185a6f941229656e1b74
Kiran Namawar <kirannamawar@gmail.com>
2023-11-22 22:08:27 +0530

    Update background color and add random color generator function

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function colorise() {
   const color = document.querySelector("article");
-  color.style.backgroundColor = "maroon";
+  color.style.backgroundColor = getRandomColor();
   color.style.borderRadius = "10%";
   color.style.padding = "20px";
+  
   const about = document.querySelector("h2");
   about.style.border = "2px solid black";
+
+  const tryAgain = document.querySelector("button");
+  tryAgain.innerHTML = "Try Again";
 }
+
 
