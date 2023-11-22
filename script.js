76696c0ee8010f42fa6a99e822389691783d392a
Kiran Namawar <kirannamawar@gmail.com>
2023-11-22 22:42:31 +0530

    Update button text and style

--- script.js
+++ script.js
@@ -10,10 +10,12 @@ function getRandomColor() {
 
 function colorise() {
   const color = document.querySelector("article");
-  color.style.backgroundColor = getRandomColor();
+  // color.style.backgroundColor = getRandomColor();
   color.style.border = "dashed 0.5rem";
   color.style.borderColor = getRandomColor();
   color.style.padding = "20px";
+  color.style.fontFamily ="cursive";
+  color.style.borderRadius = "20px";
 
   // const colorText = document.querySelectorAll("article p");
   // for (let i = 0; i < colorText.length; i++) {
