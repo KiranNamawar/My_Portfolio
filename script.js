004e2ef4ff653943c0695ee34127ca1ec64f4f5f
Kiran Namawar <kirannamawar@gmail.com>
2023-11-22 21:54:46 +0530

    Add colorize button to About Me section

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-
-// Function to display a welcome message
-function displayWelcomeMessage() {
-  console.log("Welcome to my portfolio!");
+function colorise() {
+  const color = document.querySelector("article");
+  color.style.backgroundColor = "maroon";
+  color.style.borderRadius = "10%";
+  color.style.padding = "20px";
+  const about = document.querySelector("h2");
+  about.style.border = "2px solid black";
 }
 
-// Function to calculate the total number of projects
-function calculateTotalProjects(projects) {
-  return projects.length;
-}
-
-// Array to store project names
-var projects = ["Project 1", "Project 2", "Project 3"];
-
-// Display the welcome message
-displayWelcomeMessage();
-
-// Calculate and display the total number of projects
-var totalProjects = calculateTotalProjects(projects);
-console.log("Total projects: " + totalProjects);
