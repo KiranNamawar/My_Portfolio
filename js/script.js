71b2d4d49a8562bd6331c0297ec8aec0c1029410
Kiran Namawar <kirannamawar@gmail.com>
2023-11-25 00:27:09 +0530

    Fix button function and style

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-
 // Get Random Color function
 function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
+  const letters = "0123456789ABCDEF";
+  let color = "#";
   for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
@@ -10,24 +9,23 @@ function getRandomColor() {
 }
 //----------------------------------------------------//
 
-
-function colorise() {
+function colorize() {
   const color = document.querySelector("article");
   // color.style.backgroundColor = getRandomColor();
   color.style.border = "dashed 0.5rem";
   color.style.borderColor = getRandomColor();
   color.style.padding = "20px";
-  color.style.fontFamily ="cursive";
+  color.style.fontFamily = "cursive";
   color.style.borderRadius = "20px";
-  color.style.fontSize = "1.5rem"
+  color.style.fontSize = "1.5rem";
 
-
-  const colorText = document.querySelectorAll("article p").forEach((p) => p.style.color = getRandomColor());
+  const colorText = document
+    .querySelectorAll("article p")
+    .forEach((p) => (p.style.color = getRandomColor()));
 
   const colorTitle = document.querySelector(".about");
   colorTitle.style.color = getRandomColor();
 
-  
   const about = document.querySelector("h2");
   about.style.border = "4px dotted";
   about.style.borderColor = getRandomColor();
@@ -38,5 +36,3 @@ function colorise() {
   tryAgain.style.fontSize = "1.5rem";
   tryAgain.style.padding = "0.2rem 0.7rem";
 }
-
-
