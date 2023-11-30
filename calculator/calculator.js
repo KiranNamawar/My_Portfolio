c62327092cd0829dca672080fa8b8c77c08ea92e
Kiran Namawar <kirannamawar@gmail.com>
2023-11-30 23:33:09 +0530

    Update calculator height and button width

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,6 +6,15 @@ function appendOperator(operator) {
   document.getElementById("result").value += operator;
 }
 
+function appendDecimal() {
+  document.getElementById("result").value += ".";
+}
+
+// function calculatePercentage(value, percentage) {
+
+//   return (value * percentage) / 100;
+// }
+
 function calculate() {
   var result = eval(document.getElementById("result").value);
   document.getElementById("result").value = result;
